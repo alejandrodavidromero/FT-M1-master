c48ef19af4a2c9f5f8b06041132935b2330c9cab
alejandrodavidromero <romero.alejandrodavid@gmail.com>
2022-08-13 14:01:20 -0300

    classe 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,17 +1,25 @@
-'use strict'
+'use strict';
+
+const { arrayReplaceAt } = require("markdown-it/lib/common/utils");
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  num = num.split("").reverse;
+  for (let i = 0; i < num.length; i++) {
+    result = result + Math.pow(2, i) * parseInt(num[i]);
+  }
+  return result;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  while (num > 0) {
+    result.push(num % 2);
+    num = Math.floor(num / 2);
+  }
+  return result.reverse().join("");
 }
 
 
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
-}
+};
