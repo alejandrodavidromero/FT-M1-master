25f31fee723753fa4966c440d5b62fbd2587daf2
alejandrodavidromero <romero.alejandrodavid@gmail.com>
2022-08-25 18:37:40 -0300

    tarea07

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 // No cambies los nombres de las funciones.
 
 function factorear(num) {
@@ -6,8 +6,17 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let resultado = [1];
+  for (let i = 2; i <= num; i++) {
+    if (num % i === 0) {
+      resultado.push(i);
+      num /= i;
+      i -= 1;
+    }
+  }
+  return resultado;
 }
+
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
@@ -15,6 +24,16 @@ function bubbleSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let aux = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = aux;
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -23,7 +42,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 1; i < array.length; i++) {
+    let value = array[i];
+    let aux = i - 1;
+    while (aux >= 0 && array[aux] > value) {
+      array[aux + 1] = array[aux];
+      aux--;
+    }
+    array[aux + 1] = value;
+  }
+  return array;
 }
 
 
@@ -32,7 +60,18 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    let menor = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[menor]) {
+        menor = j;
+      }
+    }
+    if (menor !== i) {
+      [array[i], array[menor]] = [array[menor], array[i]];
+    }
+  }
+  return array;
 }
 
 
